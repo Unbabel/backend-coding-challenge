78c1e782fb8bffb90a9ca3d997b6c1e30dd69a15
aabramos <aalbertobramos@gmail.com>
2018-09-12 19:54:39 -0300

    Added basic translation form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
 from flask import render_template
+from app.forms import UnbabelForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Unbabel Coding Challenge')
+    form = UnbabelForm()
+    return render_template('index.html', title='Unbabel Coding Challenge', form=form)
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = '\x86\xd8Z\x94\xd7\x12o}\x18\xf28\xf5\x17~\xbf\x14\xc9\x8b\xa1b\x81\xb8\x88\xb4'
