01be2bc0a80defa34cf24b01cb1a28fa38d605be
aabramos <aalbertobramos@gmail.com>
2018-09-13 20:02:19 -0300

    Flask shell, changes for models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class Translation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    source_text = db.Column(db.String(255))
-    translated_text = db.Column(db.String(255))
-    uid = db.Column(db.String(20), unique=True)
-    status = db.Column(db.String(15))
+    source_text = db.Column(db.String(255), nullable=False)
+    translated_text = db.Column(db.String(255), nullable=False)
+    uid = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(15), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,18 @@
 from app import app
 from flask import render_template
 from app.forms import UnbabelForm
+from config import Config
 import requests
 
 
-url = 'https://sandbox.unbabel.com/tapi/v2/translation/'
-headers = {
-    'Authorization': 'ApiKey fullstack-challenge:9db71b322d43a6ac0f681784ebdcc6409bb83359',
-    'Content-Type': 'application/json',
-}
 payload = {
     'text': 'Hello world',
     'source_language': 'en',
     'target_language': 'pt',
     'text_format': 'text',
 }
-r = requests.post(url, json=payload, headers=headers)
-print(r)
+# r = requests.post(Config.URL, json=payload, headers=Config.HEADERS)
+# print(r)
 
 
 @app.route('/')
