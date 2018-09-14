ce725b5c2a94a93c8f8217bf8b89580f97f86d08
aabramos <aalbertobramos@gmail.com>
2018-09-14 20:49:09 -0300

    creating view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
+from flask.views import MethodView
 from app.forms import UnbabelForm
+
 
 bp = Blueprint('app', __name__)
 
@@ -9,3 +11,13 @@ bp = Blueprint('app', __name__)
 def index():
     form = UnbabelForm()
     return render_template('index.html', title='Unbabel Coding Challenge', form=form)
+
+
+class Index(MethodView):
+    def post(self):
+        form = UnbabelForm()
+        if form.validate_on_submit():
+            add.delay(4, 15)
+
+
+from app.tasks import add
